a63e352cf884bcdf7014d7411b606db884e64e2d
Anika Iyer <anika.sule.iyer@gmail.com>
2022-08-30 13:53:35 -0700

    Eldritch Horror Giga Pudding
    Exactly what is sounds like. 1000 times. Enjoy.

---
+++ src/Please_Let_This_Work.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Please_Let_This_Work {
+
+	public static void main(String[] args) {
+		
+			for(int i = 0; i < 1000; i++)
+			{
+				System.out.println("GIGA PUDDING");
+				System.out.println("				________________");
+				System.out.println("			      /	________________ \\");
+				System.out.println("			     /			  \\");
+				System.out.println("			    /  ___   	     ___   \\");
+				System.out.println("			   /  | _<	    | _<    \\");
+				System.out.println("		          /		-            \\");
+				System.out.println("			 /	    __________        \\");
+				System.out.println("			/	    \\_______/	       \\");
+				System.out.println("		       /		  		\\");
+				System.out.println("		      /					 \\");
+				System.out.println("		     /					  \\");
+				System.out.println("	            ————————————————————————————————————————");
+			}
+			
+	}
+
+}
